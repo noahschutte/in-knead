dd87cc90aac8bbb9a852d40e5a59a7a6b017a743
Noah Schutte <noahschutte@gmail.com>
2017-08-27 20:20:12 -0600

    remove reference to other pizza communities

--- src/components/RequestDonate.js
+++ src/components/RequestDonate.js
@@ -7,7 +7,7 @@ export default class RequestDonate extends Component {
       	<div className='request-donate-text-container'>
 	      	<h2 className='text-center text-uppercase'><b>Be a requester</b></h2>
 	      	<p className='text-center'>
-            If you’ve ever requested a pizza through the RAoP community,
+            If you’ve ever requested a pizza donation site,
             you know how many hoops you have to jump through just to make sure your food gets to you.
             in knead cuts through all the red tape to take all that stress off your shoulders.
           </p>
